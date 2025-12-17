a35872a982aafe4acc600adc77cb4bfb1b6094d5
Igor231321 <chernobay.i2112@gmail.com>
2025-12-17 08:59:55 +0200

    Updated models

--- app_run/models.py
+++ app_run/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 class Run(models.Model):
     class Status(models.TextChoices):
-        INIT = 'IN', 'init'
-        IN_PROGRESS = 'PR', 'in_progress'
-        FINISHED = 'FN' 'finished'
+        INIT = 'init', 'init'
+        IN_PROGRESS = 'in_progress', 'in_progress'
+        FINISHED = 'finished' 'finished'
 
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
-    status = models.CharField(max_length=15, choices=Status, default=Status.INIT)
+    status = models.CharField(max_length=25, choices=Status, default=Status.INIT)
 
     def __str__(self):
         return self.comment
